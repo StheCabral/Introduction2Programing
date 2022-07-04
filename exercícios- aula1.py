8f9ab48dd7efe5f379bd283a89e1a21e0970606d
Sthefany Cabral <63838200+StheCabral@users.noreply.github.com>
2022-07-04 09:49:34 -0300

    uploading some practice in class

---
+++ exercícios- aula1.py
@@ -0,0 +1,20 @@
+numero1 = int(input("Digite o primeiro número: "))
+numero2 = int(input("Digite o segundo número: "))
+
+if numero1 <= numero2:
+    print(numero1, numero2)
+else:
+     print(numero2, numero1)
+     
+  
+######
+a = int(input("Digite um número: "))
+b = int(input("Digite um número: "))
+temp = 0
+
+if (a > b):
+    temp = a
+    a = b
+    b = temp
+
+print( "Os valores foram trocados:" + str(a) + srt(b) )

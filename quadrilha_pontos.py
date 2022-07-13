8edb6052fdbf800b48d45c9fe5705684f8368a1a
Sthefany Cabral <63838200+StheCabral@users.noreply.github.com>
2022-07-13 15:23:49 -0300

    lista 1

--- quadrilha_pontos.py
+++ quadrilha_pontos.py
@@ -5,24 +5,92 @@ step3 = input()
 step4 = input()
 step5 = input()
 
-socore = 0
+score = 0
+marriage = 0
+eliminated = ""
 
-if (step 1 == "Comprimento"):
+
+if (step1 == "Cumprimento" or step1 == "Serrote"):
     score += 100
+elif (step1 == "Balancê"):
+    score += 50
+elif (step1 == "Passeio"):
+    score += 70
+elif (step1 == "Túnel" or step1 == "Despedida"):
+    score = score
+elif (step1 == "Casamento"):
+    marriage +=1
+elif(step1 != "Despedida"):
+    eliminated = "yes"
 
+if (step2 == "Cumprimento"):
+    score += 10
+elif (step2 == "Balancê"):
+    score += 50
+elif (step2 == "Passeio"):
+    score += 70
+elif (step2 == "Túnel"):
+    score -= (10/100) * score
+elif (step2 == "Serrote"):
+     score += 100
+elif (step2 == "Casamento"):
+    marriage +=1
+elif(step2 != "Despedida"):
+    eliminated = "yes"
 
+if (step3 == "Cumprimento"):
+    score += 10
+elif (step3 == "Balancê"):
+    score += 50
+elif (step3 == "Passeio"):
+    score += 70
+elif (step3 == "Túnel"):
+    score -= (10/100) * score
+elif (step3 == "Serrote"):
+    score += 100
+elif (step3 == "Casamento"):
+    marriage +=1
+elif(step3 != "Despedida"):
+    eliminated = "yes"
+
+if (step4 == "Cumprimento"):
+    score += 10
+elif (step4 == "Balancê"):
+    score += 50
+elif (step4 == "Passeio"):
+    score += 70
+elif (step4 == "Túnel"):
+    score -= (10/100) * score
+elif (step4 == "Serrote"):
+    score += 100
+elif (step4 == "Casamento"):
+    marriage +=1
+elif(step4 != "Despedida"):
+    eliminated = "yes"
+
+if (step5 == "Cumprimento"):
+    score += 10
+elif (step5 == "Balancê"):
+    score += 50
+elif (step5 == "Passeio"):
+    score += 70
+elif (step5 == "Túnel"):
+    score -= (10/100) * score
+elif (step5 == "Serrote"):
+    score += 100
 elif (step5 == "Despedida"):
     score += 35
+elif (step5 == "Casamento"):
+    marriage +=1
+else:
+    eliminated = "yes"
 
 
+if (marriage > 0):
+    score = score * 2
 
 
-
-Cada quadrilha tem uma sequência de 5 passos.
-Se algum passo diferente dos divulgados da lista for usado, a quadrilha terá a pontuação zerada.
-Caso o “Cumprimento” apareça em alguma outra posição, damos 10 pontos.
-A cada “Balancê” a pontuação é acrescida de 50 pontos.
-Cada “Passeio” vale 70 pontos.
-Cada vez que fizer o “Túnel” perde 10% da pontuação.
-“Serrote” vale 100 pontos.
-Caso o casamento seja realizado ao menos uma vez, a pontuação final é dobrada.
+if (eliminated == "yes"):
+    print(f"Poxa, {name}. Por utilizarem de algum passo não permitido vocês tiveram a pontuação zerada.")
+else:
+    print(f"Parabéns, {name}! Bela apresentação. A pontuação foi de {score:.1f}!")
---
+++ whoisdriving.py
@@ -0,0 +1,69 @@
+name1 = input()
+score1 = int(input())
+name2 = input()
+score2 = int(input())
+name3 = input()
+score3 = int(input())
+
+st_place = ""
+st_score = 0
+nd_place = ""
+nd_score = 0
+rd_place = ""
+rd_score = 0
+
+if (score1 < score2):
+    st_place = name1
+    st_score = score1
+    nd_place = name2
+    nd_score = score2
+elif (score2 < score1):
+     st_place = name2
+     st_score = score2
+     nd_place = name1
+     nd_score = score1
+else:
+    if(name1 < name2):
+        st_place = name1
+        st_score = score1
+        nd_place = name2
+        nd_score = score2
+    else:
+        st_place = name2
+        st_score = score2
+        nd_place = name1
+        nd_score = score1
+
+if (score3 < nd_score):
+        rd_place = nd_place
+        rd_score = nd_score
+        if (score3 < st_score):
+            nd_place = st_place
+            nd_score = st_score
+            st_place = name3
+            st_score = score3
+        elif (score3 > st_score):
+            nd_place = name3
+            nd_score = score3
+        else:
+            if(name3 < st_place):
+                nd_place = st_place
+                nd_score = st_place
+                st_place = name3
+                st_score = score3
+elif (score3 > nd_score):
+    rd_place = name3
+    rd_score = score3
+else:
+    if(name3 < nd_place):
+         rd_place = nd_place
+         rd_score = nd_score
+         nd_place = name3
+         nd_score = score3    
+    else:
+        rd_place = name3
+        rd_score = score3
+
+print(f"{st_place} {st_score}")
+print(f"{nd_place} {nd_score}")
+print(f"{rd_place} {rd_score}")

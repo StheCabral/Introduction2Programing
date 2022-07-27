4038148583e6d1cb68d23050244104ffd2bd3f67
Sthefany Cabral <63838200+StheCabral@users.noreply.github.com>
2022-07-27 15:01:00 -0300

    lista 2

---
+++ penny_concierge.py
@@ -0,0 +1,81 @@
+previous_call = "none"
+
+while (call != "Boa noite"):
+    call = input()
+    if (previous_call == "toc-toc-toc" and call == "Penny"):
+        i +=1
+        print(i)
+    elif (call =! "toc-toc-toc" and call!= "Penny"):
+        print("Não pode entrar, se identifique!!!")
+
+else:
+    print("Boa noite Penny")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+call = None
+previous_call = None
+i = 0
+while (call != "Boa noite"):
+    for i in range (1,7,2):
+        call = input()
+        while (previous_call == "toc-toc-toc" and call == "Penny"):
+
+                  
+        else:
+            print("Não pode entrar, se identifique!!!")

89657d8577ffa1a5c86fcbcdc22770a47144bb52
Sthefany Cabral <63838200+StheCabral@users.noreply.github.com>
2022-08-02 14:37:27 -0300

    nobel persue working list 2

--- nobel_persue.py
+++ nobel_persue.py
@@ -1,51 +1,55 @@
-
-
-# se receber "é o fim da estrada..." antes de trabalhar na caltech deve imprimir "que potencial..."
-# se ele desistir na caltech ou string theory deve imprimir "tao perto mas tao longe..."
-# se estiver no chancellor ou no cooper printar "não dessitia sehldon foi..."
-# se estiver na assimetria e n tiver ganho o nbel ainda "naoooooo...."
-# se ele ganhar o nobel "voce conseguiu..."
-
 path = 0
 step = None
-nobel = False
+nobel = 0
 give_up = False
 step_position = 0
+bazinga_power = False
 
-while (step != "É o fim da Estrada pra Sheldon Cooper" or not(nobel)):
+while (step != "É o fim da Estrada pra Sheldon Cooper" and step != "Ganhar o Nobel"):
     step = input()
-    step_position += 1
-
 
     #verificando se ele desistiu
     if (step == "É o fim da Estrada pra Sheldon Cooper"):
         give_up = True
-        step_position -= 1
         bazinga_power = False
+
+        #Considerando o ponto em que ele desistiu
+        if (nobel == 0):
+            print("Que potencial desperdiçado...")
+        elif (nobel == 1 or nobel == 2):
+            print("Tão perto, mas tão longe")
+        elif (nobel == 3 or nobel == 4):
+            print("Não desista Sheldon, você ainda têm muito para alcançar!")
+        elif (nobel == 5):
+            print("Nãoooooo, esse momento ia ser seu Sheldon")
+
     
     # se xingar os amigos deve repreender
     elif (step == "Tinha que ser o engenheiro sem Phd do Wolowitz" or step == "Leonard seu anão covarde" or step == "Tu é muito burro Raj"):
             print("Não xingue seus amigos Sheldon!")
-            step_position -= 1
             bazinga_power = False
     
     #se o próximo step for "Banzinga" o anterior será desconsiderado
     elif (step == "Bazinga"):
-        step_position -= 1 #desconsiderando o passo bazinga
         if (bazinga_power):
             step_position -= 1  # desconsiderando avanço na carreira se necesário
+            nobel -= 1
+            bazinga_power = False
 
     else:
         #Começou a Trabalhar na Caltech primeiro?
         if (step == "Começou a Trabalhar na Caltech"):
+            step_position +=1
             if (step_position == 1):
                 bazinga_power = True
                 nobel = 1
+               
             else: 
                 step_position -= 1
 
         #Trabalho sobre a String Theory segundo?
         elif (step == "Trabalho sobre a String Theory"):
+            step_position +=1
             if (step_position == 2):
                 bazinga_power = True
                 nobel = 2
@@ -54,6 +58,7 @@ while (step != "É o fim da Estrada pra Sheldon Cooper" or not(nobel)):
 
         #Ganhar o Chancellor de ciência terceiro?
         elif (step == "Ganhar o Chancellor de ciência"):
+            step_position +=1
             if (step_position == 3):
                 bazinga_power = True
                 nobel = 3
@@ -62,6 +67,7 @@ while (step != "É o fim da Estrada pra Sheldon Cooper" or not(nobel)):
 
         #Pensar na Teoria de Cooper-Hofstader quarto?
         elif (step == "Pensar na Teoria de Cooper-Hofstader"):
+            step_position +=1
             if (step_position == 4):
                 bazinga_power = True
                 nobel = 4
@@ -70,6 +76,7 @@ while (step != "É o fim da Estrada pra Sheldon Cooper" or not(nobel)):
 
         #Criou a Super Assimetria quinto?
         elif (step == "Criou a Super Assimetria"):
+            step_position +=1
             if (step_position == 5):
                 bazinga_power = True
                 nobel = 5
@@ -77,7 +84,8 @@ while (step != "É o fim da Estrada pra Sheldon Cooper" or not(nobel)):
                 step_position -= 1
 
         #Ganhar o Nobel sexto?
-        elif (step == "Ganhar o Nobel"):
+        elif (step == "Ganhar o Nobel"): 
+            step_position +=1
             if (step_position == 6):
                 bazinga_power = True
                 nobel = 6
@@ -85,30 +93,7 @@ while (step != "É o fim da Estrada pra Sheldon Cooper" or not(nobel)):
                 step_position -= 1
         #caso seja um step não listado
         else:
-            step_position -= 1
             bazinga_power = False
+    #seguiu o cronograma e ganhou o notion
     if (nobel == 6):
         print("Você conseguiu Sheldon, o Nobel é seu!!!")
-
-
-
-
-
-
-
-
-#Começou a Trabalhar na Caltech
-#Trabalho sobre a String Theory
-#Ganhar o Chancellor de ciência
-#Pensar na Teoria de Cooper-Hofstader
-#Criou a Super Assimetria
-#Ganhar o Nobel
-# receber na ordem certa
-#bazinga assim que avançar um feito este deve ser desconsiderado
-# se n for assim que avançar ta de boa
-# se receber os feitos na ordem errada deve desconsiderar
-# se receber "é o fim da estrada..." antes de trabalhar na caltech deve imprimir "que potencial..."
-# se ele desistir na caltech ou string theory deve imprimir "tao perto mas tao longe..."
-# se estiver no chancellor ou no cooper printar "não dessitia sehldon foi..."
-# se estiver na assimetria e n tiver ganho o nbel ainda "naoooooo...."
-# se ele ganhar o nobel "voce conseguiu..."

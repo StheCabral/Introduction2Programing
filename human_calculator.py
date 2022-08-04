bf2e0ef5f2f3f9c61dae38af17a3bddde164aeb6
Sthefany Cabral <63838200+StheCabral@users.noreply.github.com>
2022-08-04 11:03:37 -0300

    list 2

--- human_calculator.py
+++ human_calculator.py
@@ -1,16 +1,17 @@
 #Número de entradas
 rounds = int(input())
-length = 0
-power = 0
 score = 0
 
 for i in range (rounds):
     type = input() #tipo de conversão
     result_int = 0
     result_str = ""
+    power = 0
+    length = 0
 
     if (type == "DEC"): # Binário para Decimal
         binary = input()
+        guess = int(input())
         for element in (binary): #descobrindo quantos elementos tem no binário
             length += 1
         j = length - 1
@@ -18,44 +19,40 @@ for i in range (rounds):
             result_int +=  (int(element) * (2**j))
             j -= 1
         #Analisando a resposta de Sheldon
-        guess = input()
-        if (guess != result_int):
-            print(f"Palpite Incorreto, o valor {binary} = {result_int}")
-        else:
+        if (guess == result_int):
             score += 1
+        else:
+            print(f"Palpite Incorreto, o valor {binary} = {result_int}")
 
     elif (type == "BIN"): # Decimal para binário
         decimal = int(input())
-        assistant = decimal
-        #if (decimal % 2 > 0): #decimal é ímpar _ _ _ 1
-           # assistant -= 1
+        guess = input()
+        decimal_storage = decimal
 
         # Quantas potencias de 2 cabe nesse número decimal
-        while (assistant > 0):
-            while (assistant % 2 == 0):
-                assistant = assistant / 2
+        while (decimal_storage > 0):
+            if (decimal_storage % 2 == 0):
+                decimal_storage = decimal_storage / 2
                 power += 1
             else:
-                assistant -= 1
-
+                decimal_storage -= 1
 
         # Convertendo para binário
-        assistant2 = decimal
-        print(power)
+        decimal_storage2 = decimal
         for y in range (power, -1, -1):
-            if ((assistant2 / 2**y) >= 1):
-                result_str = (result_str + "1") 
-            else:
-                result_str = (result_str + "0")
-            assistant2 -= (assistant2 / 2 ** y)
+                if (decimal_storage2 >= (2**y)):
+                    result_str = (result_str + "1") 
+                    decimal_storage2 -= (2**y)
+                elif (decimal_storage2  < 2**y):
+                    result_str = (result_str + "0")
+  
         #Analisando a resposta de Sheldon
-        guess = input()
-        if (guess != result_str):
-            print(f"Palpite Incorreto, o valor {decimal} = {result_str}")
-        else:
+        if (guess == result_str):
             score += 1
-
-if ((score / rounds) >= 0,5): #Onde fica o igual a 50%?????
+        else:
+            print(f"Palpite Incorreto, o valor {decimal} = {result_str}")
+#Analisando resultado final do jogo
+if ((score / rounds) > 0.5): #Onde fica o igual a 50%?????
     print("Bazinga! Quem realizou esses cálculos foi o computador??")
 else: 
     print("Parece que realizar conversões em binário não é o seu forte")

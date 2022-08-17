20ca4c0afa2cf74bf51d4af4334df86b9fe1c113
Sthefany Cabral <63838200+StheCabral@users.noreply.github.com>
2022-08-17 14:57:47 -0300

    list 3

--- music.py
+++ music.py
@@ -4,14 +4,16 @@ dean_grades = []
 sam_grades = []
 for artist in artists:
     songs = input().split(" - ")
-    while ("Sam" in comment or "Dean" in comment): #rodar até receber outra música
-        enter = input()
-        if (":" in enter):
-            comment = enter.split(": ")
-            if ("Sam" in comment):
-                sam_grades.append(comment)
-            elif ("Dean" in comment):
-                dean_grades.append(comment)
-        else:
-            songs = enter.split(" - ")
-
+    for song in songs:
+        while ("Sam" in comment or "Dean" in comment): #rodar até receber outra música
+            enter = input()
+            if (":" in enter):
+                comment = enter.split(": ")
+                if ("Sam" in comment):
+                    sam_grades.append(comment)
+                elif ("Dean" in comment):
+                    dean_grades.append(comment)
+            else:
+                songs = enter.split(" - ")
+# ao inves de criar arrays de comentários, guardar o ultimo valoe de sam, o ultimo valor de dean e depois analisar
+e depois guardar os seguintes ultimos

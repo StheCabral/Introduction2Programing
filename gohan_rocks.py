1dce74e5a6752c8cf80f6479ed6da249d5baa3a2
Sthefany Cabral <63838200+StheCabral@users.noreply.github.com>
2022-09-19 10:56:34 -0300

    list 6

--- gohan_rocks.py
+++ gohan_rocks.py
@@ -1,20 +1,26 @@
 rounds = int(input())
-piccolo = [int(x) for x in input().split(" ")]
 gohan = [int(x) for x in input().split(" ")]
-gohan_d = {}
-piccolo_d = {}
+piccolo = [int(x) for x in input().split(" ")]
 gohan_win = 0
-for i in range (rounds):
-    piccolo_d[i] = piccolo[i]
-    gohan_d[i] = gohan[i]
-for key in gohan_d.keys():
-    for j in range (rounds):
-        if (gohan_d[key] == piccolo_d[j]):
-            gohan_d[key] = -1
-            gohan_win += 1
-            i += 1
-if (gohan_win == rounds):
-    print("Dale Gohan!")
-else:
-    print("Ih, nao foi agora, Gohan! Vamos tentar de novo semana que vem.")
+piccolo_dict = {}
+gohan_dict = {}
+for number in piccolo:
+    if (number not in piccolo_dict.keys()):
+        piccolo_dict.update({number : 1})
+    else:
+        piccolo_dict[number] += 1
+for number in gohan:
+    if (number not in gohan_dict.keys()):
+        gohan_dict.update({number : 1})
+    else:
+        gohan_dict[number] += 1
 
+for key in piccolo_dict.keys():
+    try:
+        if (piccolo_dict[key] == gohan_dict[key]):
+            gohan_win += 1
+    except KeyError:
+        print("Ih, nao foi agora, Gohan! Vamos tentar de novo semana que vem.")
+
+if (gohan_win == len(piccolo_dict)):
+    print("Dale Gohan!")

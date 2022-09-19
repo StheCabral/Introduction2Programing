01c80cd5e51da75baa5819b568e15532b9cfe7d2
Sthefany Cabral <63838200+StheCabral@users.noreply.github.com>
2022-09-19 09:05:29 -0300

    list 6

--- perfect_attack.py
+++ perfect_attack.py
@@ -4,11 +4,15 @@ target = int(input())
 players_numbers_temp = players_numbers_temp.replace("[", "")
 players_numbers_temp = players_numbers_temp.replace("]", "")
 players_numbers = [int(x) for x in players_numbers_temp.split(",")]
-
-for j in range (len(players_numbers)):
-    for i in range (1, (len(players_numbers) - 1)):
-        if ((i+j) < len(players_numbers)):
-            if ((players_numbers[j] + players_numbers[j+i]) == target):
-                result = [players_numbers[j], players_numbers[j+i]]
+dic_players = {}
+result = []
+for i in range (len(players_numbers)):
+    dic_players[i] = players_numbers[i] # {0: 1, 1: 4, 2: 6}
+for key in dic_players.keys():
+    for i in range (1, len(players_numbers)):
+        if ((key + i) < len(players_numbers)):
+            n1 = dic_players[key]
+            n2 = dic_players[key+1]
+            if ((dic_players[key] + dic_players[key+i]) == target):
+                result = [key, key+i]
 print(result)
-                
